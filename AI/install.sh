f95d68fdec6faac0dfddfd2fa1932ddf60f63ac5
arlomu <145472706+arlomu@users.noreply.github.com>
2025-08-22 10:37:44 +0000

    new

--- AI/install.sh
+++ AI/install.sh
@@ -3,7 +3,7 @@ ollama pull llama3.2:3b
 ollama pull qwen2.5-coder:1.5b
 ollama pull qwen2.5-coder:3b
 ollama pull qwen2.5-coder:7b
-ollama pukk deepseek-r1:1.5b
+ollama pull deepseek-r1:1.5b
 sleep 1
-echo "Done!"
+echo "Done! Downloading 12GB"
 sleep 1
