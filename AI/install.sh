075671bf61300a5f8cd0e6992ed18ad48e413a3d
arlomu <145472706+arlomu@users.noreply.github.com>
2025-08-22 10:30:37 +0000

    new

---
+++ AI/install.sh
@@ -0,0 +1,9 @@
+ollama pull llama3.2:1b
+ollama pull llama3.2:3b
+ollama pull qwen2.5-coder:1.5b
+ollama pull qwen2.5-coder:3b
+ollama pull qwen2.5-coder:7b
+ollama pukk deepseek-r1:1.5b
+sleep 1
+echo "Done!"
+sleep 1
